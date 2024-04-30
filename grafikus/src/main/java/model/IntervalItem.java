1d9ae03a97d6e38cb4c43a958fadc0616368b9f5
László Görömbey <laszlo.gorombey@gmail.com>
2024-04-30 13:06:37 +0200

    Project initialization

---
+++ grafikus/src/main/java/model/IntervalItem.java
@@ -0,0 +1,67 @@
+package model;
+
+/**
+ * Az IntervalItem egy absztrakt osztály
+ * olyan tárgyak viselkedésének és működésének modellezésére,
+ * amik egy adott ideig fejtik ki hatásukat. Az osztály
+ * felelőssége tudni, hogy a tárgy aktiválva van-e és meddig érvényes.
+ * Az Item absztrakt osztály leszármazottja.
+ */
+public abstract class IntervalItem extends Item {
+
+    //A tárgy aktiválva van-e vagy sem
+    protected boolean activated;
+    //Mennyi idő múlva változik a tárgy állapota aktiváltról.
+    protected int timeRemaining;
+
+    /**
+     * Az IntervalItem osztály konstruktora.
+     * Inicializál egy Item objektumot a megadott értékekkel.
+     *
+     * @param location a szoba, amiben a tárgy van
+     * @param holder a személy, akinél a tárgy van
+     * @param activated a tárgy aktiválva van-e
+     * @param timeRemaining a hátralévő idő, amíg a tárgy aktív
+     */
+    public IntervalItem(Room location, Person holder, boolean activated, int timeRemaining){
+        super(location, holder);
+        this.activated = activated;
+        this.timeRemaining = timeRemaining;
+    }
+
+    /**
+     * A tárgy aktiválása.
+     */
+    @Override
+    public void activate() {
+        activated = true;
+    }
+
+    /*
+     * Aktiváltság lekérdezése
+     */
+    public boolean isActivated(){
+        return activated;
+    }
+
+    /*
+     * Aktiváltság beállítása
+     */
+    public void setActivated(boolean activated){
+        this.activated = activated;
+    }
+
+    /*
+     * Hátralévő idő lekérdezése
+     */
+    public int getTimeRemaining() {
+        return timeRemaining;
+    }
+
+    /*
+     * Hátralévő idő beállítása
+     */
+    public void setTimeRemaining( int timeRemaining ){
+        this.timeRemaining = timeRemaining;
+    }
+}
